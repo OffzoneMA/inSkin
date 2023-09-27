03c547d6a079300ad6afb14d3f92a6f2d6a8285a
R3-da <haddan.reda.pers@gmail.com>
2023-09-27 16:07:38 +0100

    implement fetching products and display them in browsing screen FlatList

--- client/app/screens/browse/home.js
+++ client/app/screens/browse/home.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import {
+  SafeAreaView,
   View,
   StyleSheet,
   Text,
-  FlatList, // Import FlatList for displaying a list of items
+  FlatList,
+  StatusBar,
 } from "react-native";
 
 import Page from "../../components/Page";
@@ -15,31 +17,63 @@ import productActionsApi from "../../api/product_actions";
 import { useToast } from "react-native-toast-notifications";
 
 function DiscoverHome({ navigation }) {
-  //const [products, setProducts] = useState([]);
-
   const toast = useToast();
+  const [products, setProducts] = useState([]); // State to store the products
 
+  // Function to fetch all products and update the state
   const getAllProducts = async () => {
     const result = await productActionsApi.getAllProducts();
-    console.log(result.data);
     if (!result.ok) {
-      toast.show(result.data, {type: "danger"});
-      return;
+      toast.show(result.data, { type: "danger" });
+    } else {
+      toast.show(result.data.message, { type: "success" });
+      setProducts(result.data.products); // Set the products in the state
     }
-    toast.show(result.data.message, {type: "success"});
   };
 
+  // Fetch products when the component mounts
+  useEffect(() => {
+    getAllProducts();
+  }, []); // Empty dependency array to run this effect once on mount
 
-  return (
-    <Page>
-      <Heading>Browse</Heading>
-      
-    </Page>
+  // Render each product item
+  const Item = ({ item }) => (
+    <View style={styles.item}>
+      <Text>{item.barcode}</Text>
+      {/* Add more information about the product as needed */}
+    </View>
   );
+
+  // Inside the DiscoverHome component
+return (
+  <Page>
+    <Heading>Browse</Heading>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={products}
+        renderItem={({item}) => <Item item={item} />}
+        keyExtractor={item => item._id}
+      />
+    </SafeAreaView>
+  </Page>
+);
+
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
 
 export default DiscoverHome;
