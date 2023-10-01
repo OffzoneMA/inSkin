9be4275c3eff647844fc742fd38d207718326fc3
R3-da <haddan.reda.pers@gmail.com>
2023-10-01 15:49:58 +0100

    implement pull-to-refresh, and on focus refresh in products home screen

--- client/app/screens/browse/home.js
+++ client/app/screens/browse/home.js
@@ -6,6 +6,7 @@ import {
   Text,
   FlatList,
   StatusBar,
+  RefreshControl,
 } from "react-native";
 
 import Page from "../../components/Page";
@@ -16,48 +17,66 @@ import productActionsApi from "../../api/product_actions";
 
 import { useToast } from "react-native-toast-notifications";
 
+import { useFocusEffect } from "@react-navigation/native"; // Import useFocusEffect from React Navigation
+
 function DiscoverHome({ navigation }) {
   const toast = useToast();
-  const [products, setProducts] = useState([]); // State to store the products
+  const [products, setProducts] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-  // Function to fetch all products and update the state
   const getAllProducts = async () => {
-    const result = await productActionsApi.getAllProducts();
-    if (!result.ok) {
-      toast.show(result.data, { type: "danger" });
-    } else {
-      toast.show(result.data.message, { type: "success" });
-      setProducts(result.data.products); // Set the products in the state
+    try {
+      const result = await productActionsApi.getAllProducts();
+      if (!result.ok) {
+        toast.show(result.data, { type: "danger" });
+      } else {
+        toast.show(result.data.message, { type: "success" });
+        setProducts(result.data.products);
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    } finally {
+      setIsRefreshing(false); // Set refreshing state to false after data fetch is completed
     }
   };
 
-  // Fetch products when the component mounts
-  useEffect(() => {
-    getAllProducts();
-  }, []); // Empty dependency array to run this effect once on mount
+  // Fetch products when the component mounts and when the screen comes into focus
+  useFocusEffect(
+    React.useCallback(() => {
+      setIsRefreshing(true); // Set refreshing state to true when the screen comes into focus
+      getAllProducts();
+    }, [])
+  );
 
-  // Render each product item
+  const onRefresh = () => {
+    setIsRefreshing(true); // Set refreshing state to true when the user pulls down to refresh
+    getAllProducts();
+  };
+
   const Item = ({ item }) => (
     <View style={styles.item}>
       <Text>{item.barcode}</Text>
-      {/* Add more information about the product as needed */}
     </View>
   );
 
-  // Inside the DiscoverHome component
-return (
-  <Page>
-    <Heading>Browse</Heading>
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={products}
-        renderItem={({item}) => <Item item={item} />}
-        keyExtractor={item => item._id}
-      />
-    </SafeAreaView>
-  </Page>
-);
-
+  return (
+    <Page>
+      <Heading>Browse</Heading>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={products}
+          renderItem={({ item }) => <Item item={item} />}
+          keyExtractor={(item) => item._id}
+          refreshControl={
+            <RefreshControl
+              refreshing={isRefreshing}
+              onRefresh={onRefresh}
+            />
+          }
+        />
+      </SafeAreaView>
+    </Page>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -66,7 +85,7 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
