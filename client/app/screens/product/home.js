281b1fe29b849c58813c3b9605c219f3180cfe6d
R3-da <haddan.reda.pers@gmail.com>
2023-11-01 23:41:55 +0100

    add condition to show images flatlist

--- client/app/screens/product/home.js
+++ client/app/screens/product/home.js
@@ -290,11 +290,19 @@ function ProductHome({ route }) {
       <View style={{ flexDirection: "row" }}>
         <View style={{ height: 100, width: 100 }}>
           <View style={{ flex: 1, justifyContent: "center", borderRadius: 5, overflow: "hidden", backgroundColor: "gray" }}>
-            {brand && brand.image && brand.image.data && brand.image.data.data ? (
-            <Image 
-              source={{ uri: 'data:' + brand.image.contentType + ';base64,' + encode(brand.image.data.data.map(byte => String.fromCharCode(byte)).join('')) }}
-              style={{flex: 1, width: null, height: null}} 
-            />
+            {product && product.images && Array.isArray(product.images) && product.images.length > 0 ? (
+              <Icon
+                name="alert-circle-outline"
+                width={24}
+                height={24}
+                fill={theme["color-basic-600"]}
+                style={{alignSelf: "center"}}
+              />
+            ) : brand && brand.image && brand.image.data && brand.image.data.data ? (
+              <Image 
+                source={{ uri: 'data:' + brand.image.contentType + ';base64,' + encode(brand.image.data.data.map(byte => String.fromCharCode(byte)).join('')) }}
+                style={{flex: 1, width: null, height: null}} 
+              />
             ) : (
               <Icon
                 name="image-outline"
@@ -422,7 +430,7 @@ function ProductHome({ route }) {
 
           <TouchableOpacity
             onPress={()=>{
-              getBrandById();
+              console.log(product);
             }} 
             style={styles.button}
             activeOpacity={0.7} // Customize the opacity when pressed
